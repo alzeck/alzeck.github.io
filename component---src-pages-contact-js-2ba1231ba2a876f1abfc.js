f43b5b587727e3652a7e958feaca1de0624feaf5
Juan Sebastian Arboleda Polo <sebastipolo@gmail.com>
2020-10-21 16:19:02 +0200

    Updates

---
+++ component---src-pages-contact-js-2ba1231ba2a876f1abfc.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{"Cuy+":function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),c=n.n(a),l=(n("Wbzz"),n("Bl7J")),r=n("vrFN");t.default=function(){return c.a.createElement(l.a,null,c.a.createElement(r.a,{title:"Contact"}),c.a.createElement("h5",null," you can contact me here: "))}}}]);
+//# sourceMappingURL=component---src-pages-contact-js-2ba1231ba2a876f1abfc.js.map
