a64cd33b944e283b2412b8dd3422d01eac022afe
Juan Sebastian Arboleda <4lzeck@gmail.com>
2020-11-10 13:45:58 +0100

    update page

--- src/components/header.js
+++ src/components/header.js
@@ -2,13 +2,13 @@ import PropTypes from "prop-types"
 import React from "react"
 import Navbar from "react-bootstrap/Navbar"
 import Nav from "react-bootstrap/Nav"
-import Image from 'react-bootstrap/Image'
+
 
 const Header = ({ siteTitle }) => (
   <Navbar variant="dark" expand="sm" sticky="top" className="transparentNav">
     <div className="container my-1">
       <Navbar.Brand href="/">
-        <img src={require("../images/logo_big.png")} height={"40px"}/>
+        <img alt={"Alzeck-Logo"} src={require("../images/logo_big.png")} height={"40px"}/>
       </Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,28 @@
 import React from "react"
-import { Link } from "gatsby"
-
+//import { Link } from "gatsby"
+import TextLoop from "react-text-loop";
 import Layout from "../components/layout"
-import Image from "../components/image"
+//import Image from "../components/image"
 import SEO from "../components/seo"
 import { Jumbotron } from "react-bootstrap"
+
+//Icons
+import { AiFillLinkedin,AiFillGithub,AiOutlineMail } from "react-icons/ai";
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
 
     <Jumbotron fluid style={{ background: "white"}}>
-      <h1>Site under construction</h1>
+      <h3>Juan Sebastian Arboleda Polo</h3>
 
-      but you are welcome to look around.
+      <p>I'm a <TextLoop children={["Software Engineer", "Developer", "Graphical Designer", "Cybersecurity Enthusiast"]} /></p>
+      
+      <h3>
+        <a style={{ color: "black"}} href={"https://www.linkedin.com/in/juan-arboleda-8305801ba/?lipi=urn%3Ali%3Apage%3Ad_flagship3_resumebuilder%3BPp7i7l5VTuK0HygVdXihJw%3D%3D"}><AiFillLinkedin/></a>
+        <a style={{ color: "black"}} href={"https://github.com/AlZeck"}><AiFillGithub/></a>
+        <a style={{ color: "black"}}  href={"mailto:4lzeck@gmail.com"}><AiOutlineMail/></a>
+      </h3>
 
     </Jumbotron>
   </Layout>
