3291dc731f06c509fe951796ad826347f0e361cd
Juan Arboleda <alzeck@gmail.com>
2021-01-01 17:47:56 +0000

    Updates

--- component---src-pages-contact-js-172d6336ff8a6295de2d.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{"Cuy+":function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),c=n.n(a),l=n("Bl7J"),r=n("vrFN");t.default=function(){return c.a.createElement(l.a,null,c.a.createElement(r.a,{title:"Contact"}),c.a.createElement("h5",null," you can contact me here: "))}}}]);
-//# sourceMappingURL=component---src-pages-contact-js-172d6336ff8a6295de2d.js.map
